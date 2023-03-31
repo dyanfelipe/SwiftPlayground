3b0f7aec615b954de003854e7fe915ba8c857fa9
Dyan <lumbar_plaice0i@icloud.com>
2023-03-31 18:40:47 -0300

    feat: base switch case

--- SwiftPlayground.playground/Contents.swift
+++ SwiftPlayground.playground/Contents.swift
@@ -1,3 +1,19 @@
 import UIKit
 
-var greeting = "Hello, playground"
+/**
+ 
+ */
+let value = "Paulo"
+switch value {
+    
+case "true":
+    print("TRUE")
+    // MARK:  no need to add the word break for the process to stop
+    // MARK: When you add the word “fallthrough” it will continue checking even though it has already found the value
+case "false":
+    print("FALSE")
+    
+default:
+    print("does not exist")
+    // MARK: If you don't want it to do anything, I can use "break"
+}
